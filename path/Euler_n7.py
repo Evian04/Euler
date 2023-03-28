7ac81d17b7e2d314c322598fc82cbca850036807
Evan <118688666+Evian04@users.noreply.github.com>
2023-03-28 14:08:29 +0200

    Euler n°7: computing time added

--- path/Euler_n7.py
+++ path/Euler_n7.py
@@ -1,10 +1,14 @@
 from time import time_ns
-
 from additional_script.prime_related_functions import get_next_prime
+
+time_1 = time_ns()
 
 prime = 1
 
 for _ in range(10001):
     prime = get_next_prime(prime)
 
-print(f"The 10'001th is: {prime}")
+time_2 = time_ns()
+
+print(f"The 10 001 th prime is: {prime}")
+print(f"Computing time: {round((time_2 - time_1) / 10**9, 4)} seconds\n")
