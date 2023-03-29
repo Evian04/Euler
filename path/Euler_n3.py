babfc8f61f3c0947fc46636daff614b024e2a1cb
Evan <118688666+Evian04@users.noreply.github.com>
2023-03-29 11:27:18 +0200

    Euler n°3 optimized

--- path/Euler_n3.py
+++ path/Euler_n3.py
@@ -1,29 +1,32 @@
 from time import time_ns
-from additional_script.prime_related_functions import get_prime_scope
 from additional_script.accessibility_settings import time_precision
 
-def euler_n3(do_print_result) -> int:
+def euler_n3(do_print_result, number_to_test: int = 0) -> int:
     time_1 = time_ns()
+    
+    if number_to_test == 0:
+        number_to_test = 600851475143 # 600 851 475 143
 
-    number_to_test = 600851475143 # 600 851 475 143
-    i = number_to_test // 2
+    result = number_to_test
+    i = 2
 
-    primes = get_prime_scope(2, number_to_test // 2)
-    primes.reverse()
+    while True:
+        if result % i == 0:
+            result //= i
 
-    print("scope got")
-
-    for prime in primes:
-        if number_to_test % prime == 0:
-            biggest_prime = prime
+        else:
+            i += 1
+        
+        if i ** 2 > result:
+            break
 
     time_2 = time_ns()
 
     if do_print_result:
-        print(f"\nThe biggest prime factor of {number_to_test} is: {biggest_prime}\n")
+        print(f"\nThe biggest prime factor of {number_to_test} is: {result}\n")
         print(f"Computing time: {round((time_2 - time_1) / 10**9, time_precision)}\n")
     
-    return biggest_prime
+    return result
 
 if __name__ == "__main__":
-    euler_n3(True)
+    euler_n3(True, 100)
