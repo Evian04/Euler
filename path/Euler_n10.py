06394c7e126af7631e55ec822ae7973bd1fdf066
Evan <118688666+Evian04@users.noreply.github.com>
2023-03-29 10:39:20 +0200

    Euler n°10 released

---
+++ path/Euler_n10.py
@@ -0,0 +1,27 @@
+from time import time_ns
+from additional_script.accessibility_settings import time_precision
+from additional_script.prime_related_functions import get_prime_scope
+
+def euler_n10(do_print_result: bool) -> int:
+    time_1 = time_ns()
+
+    limit = 2000000
+
+    list_primes = get_prime_scope(2, limit)
+
+    print("scope got")
+
+    sum = 0
+    for prime in list_primes:
+        sum += prime
+    
+    time_2 = time_ns()
+
+    if do_print_result:
+        print(f"\nHere is the sum of all the prime numbers below {limit}: {sum}\n")
+        print(f"Computing time: {round((time_2 - time_1) / 10**9, time_precision)} seconds\n")
+    
+    return sum
+
+if __name__ == "__main__":
+    euler_n10(True)
