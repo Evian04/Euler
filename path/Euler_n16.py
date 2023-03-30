d5cc1f6f901e842055e2bb8e1f14d7554408c6b8
Evan <118688666+Evian04@users.noreply.github.com>
2023-03-30 15:18:56 +0200

    Euler n°16 released

---
+++ path/Euler_n16.py
@@ -0,0 +1,25 @@
+"""
+Find the sum of the digits of the number 2 ** 1000
+"""
+from time import time_ns
+from additional_script.accessibility_settings import time_precision
+
+def euler_n16(do_print_result: bool, n: int = 2 ** 1000) -> int:
+    time_1 = time_ns()
+
+    digits = [int(d) for d in str(n)]
+
+    sum = 0
+    for d in digits:
+        sum += d
+    
+    time_2 = time_ns()
+
+    if do_print_result:
+        print(f"\nThe sum of the digits of this number is: {sum}\n")
+        print(f"Computing time: {round((time_2 - time_1) / 10**9, time_precision)} seconds\n")
+    
+    return sum
+
+if __name__ == "__main__":
+    euler_n16(True)
