175faa2889df79e46765581f43e55d2dd9fa7a2b
Evan <118688666+Evian04@users.noreply.github.com>
2023-04-01 16:10:05 +0200

    Euler n°14 tweaking

--- path/Euler_n14.py
+++ path/Euler_n14.py
@@ -20,7 +20,7 @@ def euler_n14(do_print_result: bool, limit: int = 1000000) -> int:
             longest_chain = i
             biggest_length = length
 
-        # Since this program is pretty slow (30 - 40 seconds), the following lines of code will show the advancement in % every 10%
+        # Since this program is pretty slow (~40 seconds), the following lines of code will show the advancement in % every 10%
         # (only if the variable `do_print_result` is True)
         if do_print_result and i in [n * (limit // 10) for n in range(11)]:
             print(round(i / limit * 100), "%")
